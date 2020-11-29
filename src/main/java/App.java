02a0c7fffd852ea2ad55a36947b77a5b2c684096
bruno.guerra <gguerrabruno@outlook.com>
2020-11-29 16:16:38 -0300

    Fixes args

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,12 @@
 public class App {
 
     public static void main(String[] args) {
-        // java App multicast
-        if(args[0].equals("multicast")){
+        // java -jar jar-name multicast
+        if(args.length == 1 && args[0].equals("multicast")){
             (new Multicast()).start();
         }
-        // java App node config.txt 1 || 2 ...
-        else{
+        // java -jar jar-name node config.txt 1 || 2 ...
+        else if(args.length == 3 && args[0].equals("node")){
             Node n = new Node(args[1], args[2]);
             n.start();
         }
